625ae7f2e7f434c0281598b95b396d8f583012d9
fffan64 <ff.fan@hotmail.fr>
2024-06-02 00:09:35 +0200

    feat: add mysterybox with coin

---
+++ game_src/Scripts/coin.gd
@@ -0,0 +1,9 @@
+extends AnimatedSprite2D
+
+class_name Coin
+
+func _ready():
+	var coin_tween = get_tree().create_tween()
+	coin_tween.tween_property(self, "position", position + Vector2(0, -40), .3)
+	coin_tween.chain().tween_property(self, "position", position, .3)
+	coin_tween.tween_callback(queue_free)
